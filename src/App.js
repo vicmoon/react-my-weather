075c8fee34ec7d63262f2c45aca4d82e0a7c546d
Munteanu <munteanuvictoria1@gmail.com>
2022-11-01 11:47:22 +0100

    corrected rel

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     <div className="App">
       Weather is great
       <footer>
-      <a href='https://github.com/vicmoon/react-my-weather' target="_blank">Open source code</a> by <a href="http://victoriamunteanuportfolio.net/" target="_blank">Victoria Munteanu</a>
+      <a href='https://github.com/vicmoon/react-my-weather' target="_blank" rel="noreferrer">Open source code</a> by <a href="http://victoriamunteanuportfolio.net/" target="_blank" rel="noreferrer">Victoria Munteanu</a>
     
       </footer>
 </div>
