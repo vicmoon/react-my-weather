163436287b911371a2bdace29b8fdd842356b522
Munteanu <munteanuvictoria1@gmail.com>
2022-11-01 11:43:01 +0100

    added links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       Weather is great
-    </div>
+      <footer>
+      <a href='https://github.com/vicmoon/react-my-weather' target="_blank">Open source code</a> by <a href="http://victoriamunteanuportfolio.net/" target="_blank">Victoria Munteanu</a>
+    
+      </footer>
+</div>
   );
 }
 
-export default App;
+
