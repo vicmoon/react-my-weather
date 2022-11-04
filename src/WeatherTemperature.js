278c68247180bc016cfebda785f4b5528d810b18
Munteanu <munteanuvictoria1@gmail.com>
2022-11-04 10:09:17 +0100

    metrics update

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -5,15 +5,35 @@ import "./Weather.css"
 export default function WeatherTemperature (props){
 
 const [metrics, setMetrics]= useState("celsius");
-if( metrics === "celsius") {
+function convertFahrenheit(event) {
+    event.preventDefault();
+    setMetrics("fahrenheit")
 
+}
+
+function convertCelsius(event){
+    event.preventDefault();
+    setMetrics("celsius")
+}
+
+
+
+if( metrics === "celsius") {
     return (
         <div>
         <span>{Math.round(props.celsius)}</span>
-        <span className="units"> °C|°F</span>
+        <span className="units"> <a href="/" onClick={convertCelsius} >°C </a>|<a href="" onClick={convertFahrenheit}> °F </a> 
+        </span>
         </div>
     );
 }else {
-    return "F"
+    let fahrenheit =(props.celsius *9) /5 +32;
+    return (
+        <div>
+        <span>{Math.round(fahrenheit)}</span>
+        <span className="units"> <a href="/" onClick={convertCelsius} >°C</a>|<a href="" onClick={convertFahrenheit}> °F </a> 
+        </span>
+        </div>
+    );
 }
 }
